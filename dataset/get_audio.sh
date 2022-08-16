f61469c18e05e984cd2af0db127933395b7ceeb9
Mbataku Clinton <30688323+itsclint@users.noreply.github.com>
2022-08-16 21:14:23 +0100

    Updated get_audio.sh

--- dataset/get_audio.sh
+++ dataset/get_audio.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env zsh
 
+SECONDS=0
+
 #output directory
 outputdir=../audio
 
 
 # downloads audio files from the links in the nassvid.txt file
 youtube-dl -o ${outputdir}/"%(title)s.%(ext)s" -x --audio-format wav  -a, --batch-file "nassvid.txt"
+
+ELAPSED="$(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+
+echo "Done! Elapsed time: $ELAPSED"
